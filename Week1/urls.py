b304e4518e8abb402320d816cb34dcdeefdda618
Kshitij04Poojary <143413977+Kshitij04Poojary@users.noreply.github.com>
2023-10-04 15:57:52 +0530

    Add files via upload

--- Week1/urls.py
+++ Week1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.HomeView.as_view(),name='home'),
@@ -10,5 +12,7 @@ urlpatterns=[
     path('article/edit/<int:pk>/',views.UpdatePostView.as_view(),name='updatepost'),
     path('article/remove/<int:pk>/',views.DeletePostView.as_view(),name='deletepost'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
